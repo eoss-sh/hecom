01ed4cd8544b0b67a55e6a074394566fca051d8c
eoss-sh <sh@eoss.ch>
2026-03-28 19:31:39 +0100

    small page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "PPFraktionMonoRegular",
+    cssVariable: "--font-fraktion-mono",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/PPFraktionMonoRegular.woff2'],
+        weight: 'normal',
+        style: 'normal'
+      }]
+    }
+  }]
+});
